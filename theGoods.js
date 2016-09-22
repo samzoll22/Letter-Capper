ac351e903eda8e7e11b2a72f4a69efa77c838c5e
Sam Zoll <sam.zoll22@gmail.com>
2016-09-22 00:20:38 -0500

    Update theGoods.js

--- theGoods.js
+++ theGoods.js
@@ -5,11 +5,8 @@ var capper = str.split(" ");
 for (var i = 0; i < capper.length; i++){
     capper[i] = capper[i].substr(0,1).toUpperCase() + capper[i].substr(1);
 }
-
-  // code goes here  
   return capper.join(" "); 
          
 }
-   
-// keep this function call here 
+
 LetterCapitalize("Argument goes here");
